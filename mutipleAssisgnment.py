a040bae18659239684c6cc524b388e2cac41384a
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-02 20:52:25 -0400

    assignment

---
+++ mutipleAssisgnment.py
@@ -0,0 +1,15 @@
+#Multiple assignment
+n, m = 0, 'abc'
+print('n, m =', n, m)
+
+n, m, z = 0.125, 'abc', False
+print('n, m, z =', n, m, z)
+
+#Increment 
+n = n + 1  # good
+print('n =', n)
+
+n += 1 # good
+print('n =', n)
+
+# n++ #bad
