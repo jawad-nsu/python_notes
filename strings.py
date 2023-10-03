ca0bb6e936fa52ace120eab11eeabc22a1416a4a
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-02 22:31:57 -0400

    strings

--- strings.py
+++ strings.py
@@ -5,3 +5,9 @@ print(s[0:2])
 
 # But they are immutable
 s[0] = "A" # this will thrown an error
+
+# updating string will create a new string
+# appending `def` to the end of the string will create a new string
+# string modification is considered n time operation 
+s += "def"
+print(s)
