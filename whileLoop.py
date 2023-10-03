35da259a1e24f60e4cfa40c2047917576f15b381
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-02 20:52:48 -0400

    while loop

---
+++ whileLoop.py
@@ -0,0 +1,8 @@
+# While loops are similar in the sense that 
+# conditions do not have to into a parentheses 
+# The block is followed by a colon and the code is indented by a tab
+
+n = 0
+while n < 5:
+    print(n)
+    n +=1
