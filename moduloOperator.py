4848a50b42d7c47cfdd5e4717f53485af6c19e75
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-02 20:51:55 -0400

    modulo

---
+++ moduloOperator.py
@@ -0,0 +1,11 @@
+# Modding is similar to most languages
+print(10 % 3) 
+# >> 1
+
+# Except for negative values
+print(-10 % 3)
+# >> 2
+
+# To be consistent with other language's modulo
+import math
+print(math.fmod(-10, 3))
