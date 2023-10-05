ba50cf79e4e149b74408a29fb00b5c2d0d251dab
jawad-nsu <jawadurrahim1997@gmail.com>
2023-10-04 22:08:17 -0400

    class

---
+++ Class.py
@@ -0,0 +1,13 @@
+# Class
+class MyClass:
+    # Constructor
+    def __init__(self, nums):
+        # Create member variables
+        self.nums = nums
+        self.size = len(nums)
+
+    def getLength(self):
+        return self.size
+    
+    def getDoubleLength(self):
+        return 2 * self.getLength()
